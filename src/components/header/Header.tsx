e5cfea2d2eb71e6898a3fbb57a699dbc27476192
jonaDJ <jonathandarla255@gmail.com>
2023-10-14 21:12:45 -0700

    added nav bar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.scss";
 import git from "../../assets/icon/git-icon.svg";
 import linkedIn from "../../assets/icon/linkedIn-icon.svg";
-import location from "../../assets/icon/location-icon.svg";
-import phone from "../../assets/icon/phone-icon.svg";
 import email from "../../assets/icon/email-icon.svg";
 import WrapperComponent from "../wrapperComponent/WrapperComponent";
 
@@ -12,32 +10,67 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ name }) => {
+  const [isOpenNav, setIsOpenNav] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768); // Set mobile width as per your design
+
+  const menuClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsOpenNav(!isOpenNav);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); //mixin for breakpoint-tablet
+      if (!isMobile) {
+        setIsOpenNav(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isMobile]);
+
   return (
-    <div className="app-header">
+    <div className={`app-header ${isOpenNav ? "nav-open" : ""}`}>
       <WrapperComponent>
         <div className="header-frame">
           <div className="name">{name}</div>
-          <div className="navbar">
-            <div className="icon-links">
-              <a href="https://github.com">
-                <img src={git} alt="git" />
-              </a>
-              <a href="https://linkedin.com">
-                <img src={linkedIn} alt="linkedIn" />
-              </a>
-              <a href="your-address">
-                <img src={location} alt="Location" />
-              </a>
-              <a href="tel:+1234567890">
-                <img src={phone} alt="phone" />
-              </a>
-              <a href="mailto:your-email@example.com">
-                <img src={email} alt="email" />
-              </a>
+          <div className={`outerNav ${isOpenNav ? "outer-nav-open" : ""}`}>
+            <div className="navbar">
+              <div className="icon-links">
+                <a href="https://github.com">
+                  <img src={git} alt="git" />
+                </a>
+                <a href="https://linkedin.com">
+                  <img src={linkedIn} alt="linkedIn" />
+                </a>
+                <a href="mailto:your-email@example.com">
+                  <img src={email} alt="email" />
+                </a>
+                {isMobile && (
+                  <button onClick={menuClickHandler} className="burger-button">
+                    <div
+                      className={`burger ${isOpenNav ? "active" : ""}`}
+                    ></div>
+                    <div
+                      className={`burger ${isOpenNav ? "active" : ""}`}
+                    ></div>
+                    <div
+                      className={`burger ${isOpenNav ? "active" : ""}`}
+                    ></div>
+                  </button>
+                )}
+              </div>
             </div>
-            <a href="/">about</a>
-            <a href="/Project">Project</a>
-            <a href="/contact">Contact me</a>
+            {(isOpenNav || !isMobile) && (
+              <div className={`sideNavBar ${!isMobile ? "bigger" : ""}`}>
+                <a href="/">about</a>
+                <a href="/Project">Project</a>
+                <a href="/contact">Contact me</a>
+              </div>
+            )}
           </div>
         </div>
       </WrapperComponent>
